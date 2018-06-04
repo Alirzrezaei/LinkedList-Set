e6647f11b341bed5949f1bc4824ce9ebcf467181
ite102770 <ite102770@383cc300-1c0e-49e2-a6cb-a3da9cc689f7>
2018-06-04 08:27:24 +0000

    last version

--- grp22_ueb04/src/grp22_ueb04/Set.java
+++ grp22_ueb04/src/grp22_ueb04/Set.java
@@ -10,5 +10,34 @@ package grp22_ueb04;
  * @author Alireza
  */
 public class Set {
+    private static Element sets;
     
+    public boolean isEmpty(){
+        return sets == null;
+    }
+    
+    public boolean isSame(Set other) {
+
+        if (this.isEmpty() && other.isEmpty()) {
+            return true;
+        } else if (this.isEmpty() || other.isEmpty()) {
+            return false;
+        } else {
+            return this.isSame(other);
+        }
+    }
+    public void addElement(char value){
+        
+    }
+    public boolean existsElement(char value){
+        if(this.isEmpty()){
+            return false;
+        }
+        else{
+            return sets.existsElement(value);
+        }
+    }
+
 }
+    
+
---
+++ grp22_ueb04/test/grp22_ueb04/SetTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grp22_ueb04;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alireza
+ */
+public class SetTest {
+    
+    public SetTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
